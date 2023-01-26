0accc2d1c4ad5c3eb8e4cab8be48170804ea1820
night3098 <122676310+night3098@users.noreply.github.com>
2023-01-26 21:56:08 +0700

    Add files via upload

---
+++ encrypt.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include "encrypt.h"
+
+int main() {
+	char *msg;
+    printf("Enter text to encrypt.\n\n");
+
+	while(fgets(msg, 800, stdin)) {
+        encrypt(msg);
+        printf("%s\n\n", msg);
+	}
+	
+	return 0;
+}
+
