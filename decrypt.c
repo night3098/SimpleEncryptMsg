61740f823ecae2f47894c1a2cfd42edc7c520f8f
night3098 <122676310+night3098@users.noreply.github.com>
2023-01-26 14:47:05 +0000

    Update decrypt.c

--- decrypt.c
+++ decrypt.c
@@ -7,13 +7,8 @@ int main() {
     char *str;
     printf("Enter text to dencrypt.\nEnter q to exit\n");	
     while(fgets(str, 800, stdin)) {
-        if(strcmp(str, "q") == 0) {
-            break;
-        }
-        else {
-            decrypt(str);
-		    printf("%s", str);
-        }
-	}
+	decrypt(str);
+	printf("%s", str);
+    }
 	return 0;
 }
